14f224ae855ee60af5dcc22f78cfb5c56e2ff3eb
John Griebel <john@justgoodsoftware.io>
2023-02-14 11:45:48 -0500

    Update serializer, openapi spec
    Signed-off-by: John Griebel <john@justgoodsoftware.io>

--- pokeapi/serializers.py
+++ pokeapi/serializers.py
@@ -6,7 +6,8 @@ from .models import Pokemon
 class PokemonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pokemon
-        fields = ["id", "created_at", "updated_at", "name", "primary_type",
-                  "secondary_type", "total", "hp", "attack", "defense",
-                  "special_attack", "special_defense", "speed"]
+        fields = ["id", "creature_id", "created_at", "updated_at", "name",
+                  "primary_type", "secondary_type", "total", "hp", "attack", "defense",
+                  "special_attack", "special_defense", "speed", "generation",
+                  "legendary"]
         read_only_fields = ["id", "created_at", "updated_at"]
